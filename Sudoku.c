7c3951d0f779bdafcc79590c40ffa5660d5bfea5
Mark Anthony Ngo <32201712+MarkAnthonyNgo@users.noreply.github.com>
2017-10-04 10:23:26 -0400

    Add files via upload

---
+++ Sudoku.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include "Sudoku.h"
+
+int checker[] = {5, 3, 0, 0, 7, 0, 0, 0, 0,
+                 6, 0, 0, 1, 9, 5, 0, 0, 0,
+                 0, 9, 8, 0, 0, 0, 0, 6, 0,
+                 8, 0, 0, 0, 6, 0, 0, 0, 3,
+                 4, 0, 0, 8, 0, 3, 0, 0, 1,
+                 7, 0, 0, 0, 2, 0, 0, 0, 6,
+                 0, 6, 0, 0, 0, 0, 2, 8, 0,
+                 0, 0, 0, 4, 1, 9, 0, 0, 5,
+                 0, 0, 0, 0, 8, 0, 0, 7, 9};
+
+void print_board() {
+  int i, j, x;
+  int board[ROW][COL];
+
+  // Prints the column headings.
+  printf("    A   B   C    D   E   F    G   H   I\n");
+
+  // Reads checker[] into board[][]
+  x = 0;
+  for (i = 0; i < ROW; i++) {
+    for (j = 0; j < COL; j++) {
+      board[i][j] = checker[x];
+      x++;
+    }
+  }
+
+  // Prints board.
+  printf("  _______________________________________\n");
+
+  for (i = 0; i < ROW; i++) {
+    printf("  |   |   |   ||   |   |   ||   |   |   |\n");
+    printf("%d ", i+1);
+    for (j = 0; j < COL; j++) {
+      if (j == 3 || j ==6) {
+        if (board[i][j] == 0) {
+          printf("||   ");
+        } else {
+          printf("|| %d ", board[i][j]);
+        }
+      } else if (board[i][j] == 0) {
+        printf("|   ");
+      } else {
+        printf("| %d ", board[i][j]);
+      }
+    }
+    printf("|\n");
+    printf("  |   |   |   ||   |   |   ||   |   |   |\n");
+    if (i == 2 || i == 5) {
+      printf("  |-------------------------------------|\n");
+      printf("  |-------------------------------------|\n");
+    } else {
+      printf("  |-------------------------------------|\n");
+    }
+  }
+}
+
+int main() {
+  int userROW;
+  char userCOL;
+
+  print_board();
+
+  printf("Please select a row: ");
+  scanf("%d", &userROW);
+  while (userROW < 1 || userROW > 9) {
+    printf("Invaild Move. Please select a row: ");
+    scanf("%d", &userROW);
+  }
+
+  printf("Please select a column: ");
+  userCOL = getchar();
+/*  while (userCOL < 1 || userCOL > 9) {
+    printf("Invaild Move. Please select a column: ");
+    userCOL = getchar();
+  }*/
+}
---
+++ Sudoku.h
@@ -0,0 +1,13 @@
+#define ROW 9
+#define COL 9
+#define TRUE 0
+#define FALSE 1
+/*#define A 1
+#define B 2
+#define C 3
+#define D 4
+#define E 5
+#define F 6
+#define G 7
+#define H 8
+#define I 9*/
